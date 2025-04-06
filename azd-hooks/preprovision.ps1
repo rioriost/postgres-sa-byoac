ab654f9e7ea1db212c8db2cd5d68aa3c79ee42c2
jjfrost <jfrost@microsoft.com>
2025-04-06 14:59:43 -0700

    Fixed typo in preprovision.ps1 messages.

--- azd-hooks/preprovision.ps1
+++ azd-hooks/preprovision.ps1
@@ -4,10 +4,10 @@ $user = az ad signed-in-user show | ConvertFrom-Json
 # Set principal name based on presence of "#" in userPrincipalName
 if ($user.userPrincipalName -like "*#*" -and $user.mail) {
     $env:AZURE_PRINCIPAL_NAME = $user.mail
-    Write-Host "Found a '#' so using mail attribute: $user.mail"
+    Write-Host "Found a '#' so using mail attribute: $env:AZURE_PRINCIPAL_NAME"
 } else {
     $env:AZURE_PRINCIPAL_NAME = $user.userPrincipalName
-    Write-Host "There is no '#' so using userPrincipalName attribute: $user.userPrincipalName"
+    Write-Host "There is no '#' so using userPrincipalName attribute: $env:AZURE_PRINCIPAL_NAME"
 }
 
 # Write to azd env
