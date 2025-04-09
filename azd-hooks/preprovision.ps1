c46fcc2a71e5607891186e073214a06c4844c178
jjfrost <jfrost@microsoft.com>
2025-04-09 12:17:27 -0700

    Improved user principal name extraction to use azd auth login output, this fixes issue with multiple Entra ID principals.

--- azd-hooks/preprovision.ps1
+++ azd-hooks/preprovision.ps1
@@ -1,17 +1,26 @@
-# Get current signed-in user object
-$user = az ad signed-in-user show | ConvertFrom-Json
+# Run azd auth login --check-status and capture the output
+$userOutput = azd auth login --check-status
 
-# Set principal name based on presence of "#" in userPrincipalName
-if ($user.userPrincipalName -like "*#*" -and $user.mail) {
-    $env:AZURE_PRINCIPAL_NAME = $user.mail
-    Write-Host "Found a '#' so using mail attribute: $env:AZURE_PRINCIPAL_NAME"
-} else {
-    $env:AZURE_PRINCIPAL_NAME = $user.userPrincipalName
-    Write-Host "There is no '#' so using userPrincipalName attribute: $env:AZURE_PRINCIPAL_NAME"
+# Extract the first email address found in the output
+# The reason for this is because some users may have multiple Entra ID principals
+# associated to their logged in account, and this way we get the exact principal name
+# from which azd is logged in with.  It takes the string of the return text of the command and
+# extracts the first email address it finds in the string.
+if ($userOutput -match "[\w\.\-]+@[\w\.\-]+\.\w+")
+{
+    $email = $matches[0]
+    $env:AZURE_PRINCIPAL_NAME = $email
+
+    Write-Host "Extracted email: $env:AZURE_PRINCIPAL_NAME"
+
+    # Write to azd env
+    azd env set "AZURE_PRINCIPAL_NAME" "$env:AZURE_PRINCIPAL_NAME"
+
+    Write-Host "User Principal Name Set: $env:AZURE_PRINCIPAL_NAME"
 }
-
-# Write to azd env
-azd env set "AZURE_PRINCIPAL_NAME" "$env:AZURE_PRINCIPAL_NAME"
-
-Write-Host "User Principal Name Obtained: $env:AZURE_PRINCIPAL_NAME"
-
+else
+{
+    $errorMessage = "ERROR: No email address found in azd auth output."
+    Write-Host $errorMessage
+    throw $errorMessage
+}
